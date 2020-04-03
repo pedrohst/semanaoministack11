acac51804a879765ef808154619f3c6870280c31
pedrohst <pedrohst@gmail.com>
2020-04-03 11:46:33 -0300

    App mobile

---
+++ mobile/App.js
@@ -0,0 +1,35 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './src/routes';
+
+export default function App()
+{
+  return (
+
+    <Routes />
+
+  );
+}
+
+/* import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>HELLOOOO APP!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#bebebe',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}); */
